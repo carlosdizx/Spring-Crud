7ea63468d18f712242f1aae9fad790daa31691da
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2022-01-22 19:36:35 -0500

    Creado los srevicios genericos para las operaciones crud

---
+++ src/main/java/ejemplo/commons/GenericServiceApi.java
@@ -0,0 +1,14 @@
+package ejemplo.commons;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface GenericServiceApi<T, ID extends Serializable> {
+    T save(T entity);
+
+    void delete(ID id);
+
+    T findByID(ID id);
+
+    List<T> getAll();
+}
